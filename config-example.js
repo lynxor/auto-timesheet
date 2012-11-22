6800c9344ddc4037743e04dc26063c5c99b119b4
daafvader <dawid.malan@gmail.com>
2012-11-22 10:02:46 +0200

    add config example

---
+++ config-example.js
@@ -0,0 +1,7 @@
+//Fill in your details and rename this file to config.js
+exports.config = {
+    username: "username",
+    password: "password",
+    url: "website url",
+    proxy: process.env['http_proxy']                        //use system proxy
+}
