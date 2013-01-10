4755164e21b875e129a10eb72e01e3d133ad2242
daafvader <dawid.malan@gmail.com>
2013-01-10 16:02:55 +0200

    Add interactive mode

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var jsdom = require("jsdom"),
     jqueryString = fs.readFileSync("./vendor/jquery.js").toString(),
     _ = require("underscore"),
     Browser = require("zombie"),
+    readline = require("readline"),
     config = require("./config").config,
     browser = new Browser(
         {
@@ -15,23 +16,45 @@ var jsdom = require("jsdom"),
                 'Accept-Encoding':'gzip, deflate',
                 'Proxy-Connection':'keep-alive'},
 
-            proxy: config.proxy,
+            proxy:config.proxy,
             runScripts:true,
-            maxWait: 10000,
-            waitFor: 5000
+            maxWait:10000,
+            waitFor:5000
         }),
     url = config.url;
 
-function weekEntries(week) {
-    week = week || moment();
+function weekEntries(week, entryFunc, hours) {
     return _.map(_.range(1, 6), function (day) {
-        return {
-            date:moment(week).day(day),
-            hours:8,
-            description:"Wrote some crappy code"
-        }
+        return entryFunc(moment(week).day(day), hours);
     });
 }
+
+function leaveDay(date, hours) {
+    return {
+        date:date,
+        hours:hours,
+        description:"Leave",
+        billable:false,
+        project:"437",
+        category:"1317"
+    }
+}
+
+function normalDay(date, hours) {
+    return {
+        date:date,
+        hours:8,
+        description:"Invest web",
+        billable:true,
+        project:"149",
+        category:"261"
+    }
+}
+
+//
+//browser.on('error', function(err){
+//    console.log("Something went wrong " + err);
+//});
 
 function start(username, pwd, entries) {
     browser.visit(url, function () {
@@ -43,6 +66,7 @@ function start(username, pwd, entries) {
                 browser.wait(function () {
                     async.forEachSeries(entries, safeAddEntry, function () {
                         console.log("done");
+                        process.exit(1);
                     });
                 });
             });
@@ -74,29 +98,32 @@ function safeAddEntry(entry, callback) {
 
 //only called once logged in
 function addEntry(entry, callback) {
-        console.log("Adding entry - " + JSON.stringify(entry));
-        browser.fill('EntryDate_TextBox', entry.date.format('DD-MMM-YYYY'))
-            .select('Project_DropDown', '149')
-            .fill('Duration_TextBox', entry.hours)
-            .check('isBillable_CheckBox')
-            .fill('Description_textBox', entry.description);
+    console.log("Adding entry - " + JSON.stringify(entry));
+    browser.fill('EntryDate_TextBox', entry.date.format('DD-MMM-YYYY'))
+        .select('Project_DropDown', entry.project)
+        .fill('Duration_TextBox', entry.hours)
+        .fill('Description_textBox', entry.description);
 
-        browser.evaluate("theForm.__EVENTTARGET = document.getElementById('__EVENTTARGET')");
-        browser.evaluate("theForm.__EVENTARGUMENT = document.getElementById('__EVENTARGUMENT')");
-        browser.evaluate("document.getElementById('EntryDate_TextBox').onchange();");
+    if (entry.billable) {
+        browser.check('isBillable_CheckBox');
+    }
+
+    browser.evaluate("theForm.__EVENTTARGET = document.getElementById('__EVENTTARGET')");
+    browser.evaluate("theForm.__EVENTARGUMENT = document.getElementById('__EVENTARGUMENT')");
+    browser.evaluate("document.getElementById('EntryDate_TextBox').onchange();");
+    browser.wait(function () {
+        console.log("updated field(s). waiting ... ");
+        browser.select('Category_DropDown', entry.category);  //Software Development - have to wait - it cascades from project dropdown
         browser.wait(function () {
-            console.log("updated field(s). waiting ... ");
-            browser.select('Category_DropDown', '261');  //Software Development - have to wait - it cascades from project dropdown
-            browser.wait(function () {
-                console.log("pressing insert...");
-                browser.pressButton('Insert', function () {
-                    console.log("Entry inserted.\n");
-                    browser.wait(function () {
-                        callback();
-                    });
+            console.log("pressing insert...");
+            browser.pressButton('Insert', function () {
+                console.log("Entry inserted.\n");
+                browser.wait(function () {
+                    callback();
                 });
             });
         });
+    });
 }
 
 //debugging
@@ -112,5 +139,63 @@ function printInputValues() {
     console.log("-------------- Inputs : " + inputs.join(" - "));
 }
 
+var args = process.argv,
+    week = moment(), //.add('weeks', 2),
+    entries = weekEntries(week, normalDay, 8);
 
-start(config.username, config.password, weekEntries());
+if (args.length > 2 && args[2] === "--dry-run") {
+
+    _.each(entries, function (e) {
+        console.log("Adding entry: " + JSON.stringify(e));
+    });
+
+    process.exit(1);
+
+} else if (args.length > 2 && args[2] === "--interactive" || args[2] === "-i") {
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    entries = [];
+
+
+    async.forEachSeries( _.range(1, 6), function(d, callback){
+        var date = moment().day(d);
+        console.log("For " + date.format("dddd DD MMM") +" : ");
+
+        rl.question("Leave (l) or Normal (n) ?", function(type){
+            rl.question("hours : ", function(hours){
+                 if(type === "l"){
+                     entries.push( leaveDay(date, parseFloat(hours)) );
+                 } else if(type === "n"){
+                     entries.push( normalDay(date, parseFloat(hours)) );
+                 } else {
+                     console.log( "Invalid type of day" );
+                 }
+                console.log("");
+                callback();
+            });
+        });
+
+
+    }, function(){
+        _.each(entries, function (e) {
+            console.log(JSON.stringify(e));
+        });
+
+        rl.question("Happy? : ", function(ans){
+            if(ans === "y"){
+                start(config.username, config.password, entries);
+            } else{
+                process.exit(1);
+            }
+        });
+    });
+
+}
+
+//start(config.username, config.password, entries);
+
+
+
